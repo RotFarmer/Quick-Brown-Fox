f7d1888011b2832e2b4df4f2cd00a76c4a3c7569
Nicole Dreon <dreon.nicole@gmail.com>
2020-10-28 15:33:22 -0400

    java2

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+let drinks = [
+  { name: "coffee", price: 1 },
+  { name: "tea", price: 2 },
+  { name: "coffee", price: 5 },
+  { name: "tea", price: 3 },
+  { name: "coffee", price: 4 },
+];
+
+let eats = [
+  {
+    name: "food",
+    price: 1,
+  },
+  {
+    name: "food",
+    price: 1,
+  },
+  {
+    name: "food",
+    price: 1,
+  },
+  {
+    name: "food",
+    price: 1,
+  },
+  {
+    name: "food",
+    price: 1,
+  },
+];
+
+let drinkButton = document.querySelector(".drink-button");
+let eatButton = doucment.querySelector(".eat-button");
+let buttonBox = document.querySelector(".button-box");
+let cart = document.querySelector(".cart");
+
+buttonBox.addEventListener("click", (e) => {
+  if (e.target.classList.contains("drink-button"){
+    ``
+  }) {
+  }else if (e.target.classList.contains("eat-button"))
+});
